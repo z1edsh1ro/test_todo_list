155719573e25729fc25225aef85cdcc3a14c0783
z1edsh1ro <ziedshero@gmail.com>
2025-03-27 14:57:18 +0700

    remove query param

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 
-const queryClient = new QueryClient();
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-        </Routes>
-      </BrowserRouter>
-  </QueryClientProvider>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Index />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
